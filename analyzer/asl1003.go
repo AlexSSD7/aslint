160998a7661e54a417e074ea20322880723f7b95
AlexSSD7 <alexandersadovskyi7@protonmail.com>
2023-03-13 19:16:01 +0100

    ASL1003+ASL1012: cover "couldn't"

--- analyzer/asl1003.go
+++ analyzer/asl1003.go
@@ -26,6 +26,7 @@ var ASL1003 = &analysis.Analyzer{
 					contains = contains || strings.HasPrefix(argStr, "cant")
 					contains = contains || strings.HasPrefix(argStr, "cannot")
 					contains = contains || strings.HasPrefix(argStr, "could not")
+					contains = contains || strings.HasPrefix(argStr, "couldn't")
 
 					if contains {
 						report(pass, arg.Pos(), "ASL1003", "use of generic error keywords like 'failed', 'unable', 'cannot' and similar is not allowed")
--- analyzer/asl1012.go
+++ analyzer/asl1012.go
@@ -38,6 +38,7 @@ var ASL1012 = &analysis.Analyzer{
 					contains = contains || strings.HasPrefix(argStr, "cant")
 					contains = contains || strings.HasPrefix(argStr, "cannot")
 					contains = contains || strings.HasPrefix(argStr, "could not")
+					contains = contains || strings.HasPrefix(argStr, "couldn't")
 
 					if contains {
 						reportFunc(argT.Pos())
