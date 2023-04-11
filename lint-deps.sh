cefde5df9e599627e7e698b479559acfb8421cb8
AlexSSD7 <alexandersadovskyi7@protonmail.com>
2023-04-11 17:46:03 +0200

    Use golangci-lint v1.51.2 commit

--- lint-deps.sh
+++ lint-deps.sh
@@ -3,7 +3,7 @@ rm -rf bin/build
 mkdir -p bin/build
 cd bin/build
 git clone https://github.com/golangci/golangci-lint .
-git reset --hard da04413a8a1eefb8c10161c9f2b558138d01815c
+git reset --hard 3e8facb4949586ba9e5dccdd2f9f0fe727a5e335 # v1.51.2
 export CGO_ENABLED=1
 make build
 cd ..; cp build/golangci-lint .
